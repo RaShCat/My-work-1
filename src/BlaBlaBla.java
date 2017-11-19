75ebf3a904c6a97df2c1d88fa8bda28fe9933105
Артём <@192.168.1.7>
2017-11-19 22:35:44 +0200

    Что - то сделал, и это работает. Скоро доработую if чаасть

--- src/BlaBlaBla.java
+++ src/BlaBlaBla.java
@@ -1,4 +1,37 @@
+import java.util.Scanner;
 
 public class BlaBlaBla {
 
-}
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Я игровой автомат!");
+		System.out.println("Запускаю баробан");
+		int a = 1;
+		int b = 6;
+		int first = a + (int) (Math.random() * b) ;
+		System.out.print(" ");
+		int second = a + (int) (Math.random() * b);
+		System.out.print(" ");
+		int third = a + (int) (Math.random() * b);
+		System.out.print(" ");
+		System.out.print(first);
+		System.out.print(" ");
+		System.out.print(second);
+		System.out.print(" ");
+		System.out.print(third);
+		System.out.print(" ");
+		if(first != second ){
+			System.out.println("Первое и второе числа не савпадают");
+      	  
+      	  }else {
+      		     if (second == third) {
+      		    	System.out.println("Джек пот");
+      		    	 
+      		                                                               }else {System.out.println("Ничья :)");       
+      		                                                                      }
+      		     } 
+      		  
+      		  
+      	  }
+      	  
+	}
